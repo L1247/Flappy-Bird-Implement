8b828a71c078d764a4f512f4179cef21dd687444
Star <starlove124760@gmail.com>
2018-04-26 19:33:04 +0800

    增加攝影機跟隨

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    private Transform _player;
+    private void Start()
+    {
+        _player = GameManager.instance.Player;
+    }
+
+    private void Update()
+    {
+        Vector3 playerPos = _player.transform.position;
+        Vector3 pos = transform.position;
+        pos.x = playerPos.x - 5;
+        transform.position = pos;
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+	[HideInInspector]public Transform Player;
+
+	public static GameManager instance;
+	// Use this for initialization
+	void Start ()
+	{
+		instance = this;
+		Player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
